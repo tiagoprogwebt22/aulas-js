3425d7f0469cc7b66c6524022e60f4dd8dd434ed
tiagoprogwebt22 <tiago.progwebt22@gmail.com>
2022-04-12 16:55:51 -0300

    Funções

---
+++ js/06-funcoes.js
@@ -0,0 +1,49 @@
+'use strict'; // MODO ESTRITO/RESTRITO
+
+/* Funções são blocos de código que
+podem ser executados e reaproveitados em
+praticamente qualquer parte da sua aplicação.
+
+Usar funções também é uma forma de separar 
+a lógica e organizar melhor sua programação.*/
+
+
+// Sintaxe tradicional
+function linha(){
+    console.log("*******");
+}
+
+let curso = "Programador Web";
+let escola = "Senac";
+let ano = 2022;
+
+console.log(`Curso: ${curso}`);
+linha();
+console.log(`Escola: ${escola}`);
+linha();
+console.log(`Ano: ${ano}`);
+
+
+              // parâmetros/argumentos
+function soma( valor1, valor2 ){
+    let total = valor1 + valor2;
+    return total;
+}
+
+console.log( soma(10, 5) );
+// console.log( total ); // erro, pois é variável local
+
+let resultado = soma(100, 50);
+console.log(resultado);
+
+if(resultado < 1000){
+    console.log("Abaixo da meta");
+}
+
+
+
+
+
+
+
+
