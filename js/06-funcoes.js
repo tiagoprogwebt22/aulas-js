accb2e097722adb7167c4dcfdba3702d5b4fdbbe
tiagoprogwebt22 <tiago.progwebt22@gmail.com>
2022-04-13 14:18:21 -0300

    Uso de Arrow Functions

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -24,12 +24,23 @@ linha();
 console.log(`Ano: ${ano}`);
 
               // parâmetros/argumentos
-function soma( valor1, valor2 ){
+
+// VERSÃO 1: sintaxe tradicional              
+/* function soma( valor1, valor2 ){
     let total = valor1 + valor2;
     return total;
-}
+} */
 
-console.log( soma(10, 5) );
+// VERSÃO 2: sintaxe ARROW FUNCTION (FUNÇÃO SETA/FLECHA)
+const soma = (valor1, valor2) => {
+    let total = valor1 + valor2;
+    return total;
+};
+
+// Arrow Function com retorno IMPLÍCITO
+const soma1 = (valor1, valor2) => valor1 + valor2;
+
+console.log( soma1(150, 55) );
 // console.log( total ); // erro, pois é variável local
 
 let resultado = soma(100, 50);
@@ -38,6 +49,55 @@ console.log(resultado);
 if(resultado < 1000){
     console.log("Abaixo da meta");
 }
+
+
+linha();
+
+// Versão 1: tradicional
+/* function dobra(numero){
+    let resultado = numero * 2;
+    return resultado;
+} */
+
+/* function dobra(numero){
+    return numero * 2;
+} */
+
+
+// Versão 2: arrow function
+/* const dobra = (numero) => {
+    let resultado = numero * 2;
+    return resultado;
+}; */
+
+/* const dobra = (numero) => { 
+    return numero * 2;
+}; */
+
+// retorno IMPLÍCITO (remove {} e return)
+// Obs.: como só temos um parâmetro (numero), pode tirar os ()
+const dobra = numero => numero * 2;
+
+console.log( dobra(10) );
+console.log( dobra(150) );
+
+linha();
+
+let nota1 = 8.5;
+let nota2 = 7.4;
+
+const calculaMedia = (n1, n2) => (n1 + n2) / 2;
+
+console.log( calculaMedia(nota1, nota2) );
+
+// Obs.: se não tem parâmetros, também precisa usar ()
+const linha2 = () => console.log("*****");
+
+
+
+
+
+
 
 
 
