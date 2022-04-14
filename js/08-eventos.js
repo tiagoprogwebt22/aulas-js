c3809e68559fc24bfb6c5c255759e58d8ad10c24
tiagoprogwebt22 <tiago.progwebt22@gmail.com>
2022-04-14 16:05:20 -0300

    Eventos e DOM
    Uso de dados de formulário

--- js/08-eventos.js
+++ js/08-eventos.js
@@ -19,4 +19,64 @@ const pagina = document.querySelector("body");
 
 botao.addEventListener("click", function(){
     pagina.classList.toggle("modo-noturno");
+
+    /* Se o texto do botão for Ativar... */
+    if ( botao.textContent == 'Ativar') {
+        // então, mudamos para Desativar:
+        botao.textContent = "Desativar";
+    } else {
+        // senão, mudamos para Ativar
+        botao.textContent = "Ativar";
+    }
 });
+
+
+/* Exemplo 03: simulação de cadastro */
+const formulario = document.querySelector("form");
+const campoNome = document.querySelector("#nome");
+const campoNota1 = document.querySelector("#nota1");
+const campoNota2 = document.querySelector("#nota2");
+const divResultados = document.querySelector("#resultados");
+
+// Detectar o acionamento do formulário
+// Obs.: a função dentro do listener, é chamada de Callback
+formulario.addEventListener("submit", function(event){
+    /* Anulando o comportamento padrão do navegador
+    (redirecionar o formulário) */
+    event.preventDefault();
+
+    // 1) Capturar os dados digitados nos campos
+    let nome = campoNome.value;
+    let nota1 = parseFloat(campoNota1.value);
+    let nota2 = parseFloat(campoNota2.value);
+
+    // 2) Calcular a média das notas
+    let media = (nota1 + nota2) / 2;
+
+    // Correção Exercício 1
+    let situacao; // inicializada
+    if (media >= 7) {
+        situacao = "aprovado";
+    } else {
+        situacao = "reprovado"
+    }
+
+    // 3) Criando elemento dinamicamente via JS
+
+    // 3.1) Criar o elemento/tag
+    let paragrafo = document.createElement("p");
+
+    // 3.2) Montar o conteúdo da tag                // Correção Exercício 2
+    paragrafo.innerHTML = `<b>${nome}</b> - <i>${media}</i> - ${situacao}`;
+
+    // 3.3) Adicionar tag/conteúdo à div de resultados
+    divResultados.appendChild(paragrafo);
+
+    /* Desafio: faça com que os campos do formulário
+    sejam resetados ao enviar. */
+    formulario.reset(); // ou this.reset()
+
+    // Devolvendo o foco (cursor) para o campo nome
+    campoNome.focus();
+});
+
